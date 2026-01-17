b6f98a3839eaa3319466c083608c6339b93034c1
Aquila Faria <aquilahenrique.silva@gmail.com>
2026-01-17 13:33:25 -0300

    fix: rename contact controller

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Contacts::class, 'index']);
+Route::get('/', [App\Http\Controllers\ContactController::class, 'index']);
